3df5db1b80fe831ed791d717cf23d7fb9b46837e
sillyy <965768042@qq.com>
2020-01-03 13:36:21 +0800

    feat svelte计数器组件demo

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	import Count from './components/count.svelte'
+	import View from './components/view.svelte'
+
+	let count = 0
+
+	function onAdd() {
+		count ++;
+	}
+	function onReduce() {
+		count --;
+	}
+</script>
+
+<main>
+	<View number={count}/>
+	<Count onAdd={onAdd} onReduce={onReduce}/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/view.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let number
+</script>
+	<p>
+        <b>计数器:</b>
+        <span>{number}</span>
+    </p>
+<style>
+    p {
+		font-size: 20px;
+		color: #333;
+		line-height: 20px;
+	}
+</style>
